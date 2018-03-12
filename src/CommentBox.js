13ac585c10e1f6a5de48469001154825e73b9354
sleeplesseditor <josh@bryson.me.uk>
2018-03-12 13:58:53 +0000

    Axios post methods added

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
-import DATA from './data';
 import './style.css';
 
 class CommentBox extends Component {
     constructor(props) {
         super(props);
         this.state = { data: [] };
+        this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+        this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
     }
+    
+    loadCommentsFromServer() {
+        axios.get(this.props.url).then(res => {
+            this.setState({ data: res.data });
+        })
+    }
+
+    handleCommentSubmit(comment) {
+
+    }
+
+    componentDidMount() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    }
+
     render() {
         return (
             <div className="commentBox">
                 <h2>Comments:</h2>
-                <CommentList data={ DATA }/>
-                <CommentForm />
+                <CommentList data={ this.state.data }/>
+                <CommentForm onCommentSubmit={ this.handleCommentSubmit }/>
             </div>
         )
     }
